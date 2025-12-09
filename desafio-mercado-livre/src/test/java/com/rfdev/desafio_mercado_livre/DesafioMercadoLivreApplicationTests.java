0a8cc36c29efc179efb46922be8b84adb5799466
Ruan Ferraz <ruanlima155@gmail.com>
2025-12-09 08:46:48 -0300

    buid: setup do projeto

---
+++ desafio-mercado-livre/src/test/java/com/rfdev/desafio_mercado_livre/DesafioMercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfdev.desafio_mercado_livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
